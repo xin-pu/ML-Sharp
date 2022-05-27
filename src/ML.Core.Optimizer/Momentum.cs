0f93eedf1d85044a81016fda781c0996beb26988
xin-pu <pu.xin@outlook.com>
2022-05-27 16:10:22 +0800

    Create Optimizer Project.

---
+++ src/ML.Core.Optimizer/Momentum.cs
@@ -0,0 +1,38 @@
+﻿using NumSharp;
+
+namespace ML.Core.Optimizer
+{
+    public class Momentum : Optimizer
+    {
+        /// <summary>
+        ///     动量法
+        /// </summary>
+        /// <param name="learningrate"></param>
+        /// <param name="rho"></param>
+        public Momentum(double learningrate, double rho = 0.9)
+            : base(learningrate)
+        {
+            Rho = rho;
+        }
+
+        /// <summary>
+        ///     动量因子
+        /// </summary>
+        public double Rho { set; get; }
+
+        /// <summary>
+        ///     负梯度的加权移动平均 => 参数更新方向
+        /// </summary>
+        public NDArray DeltaTheda { protected set; get; }
+
+        internal override NDArray call(NDArray weight, NDArray grad, int epoch)
+        {
+            if (epoch == 0)
+                DeltaTheda = np.zeros_like(weight);
+
+            DeltaTheda = Rho * DeltaTheda - WorkLearningRate * grad;
+
+            return weight + DeltaTheda;
+        }
+    }
+}
